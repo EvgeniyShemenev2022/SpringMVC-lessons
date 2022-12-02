ad50ffc2d701282f0f7846cfb7bd21b921c13984
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-12-02 20:21:31 +0300

    в параметры метода showEmpDetails добавлена аннотация @Valid; в параметры метода showEmpDetails добавлен пр-р BindingResult bindingResult

--- src/main/java/org/shemenev/spring/mvc/Controller_1.java
+++ src/main/java/org/shemenev/spring/mvc/Controller_1.java
@@ -2,11 +2,13 @@ package org.shemenev.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -26,8 +28,12 @@ public class Controller_1 {
 
     // вариант получения имени без servlet
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@ModelAttribute("employee") Employee employee) {
-        return "show-emp-details-view";
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        } else {
+            return "show-emp-details-view";
+        }
     }
 
 }
